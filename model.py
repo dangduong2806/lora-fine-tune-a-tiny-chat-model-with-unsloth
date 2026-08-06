b30e88bb66e5bf1d15866be8cc48dde90cf173de
Nguyen Dang Duong <160890193+dangduong2806@users.noreply.github.com>
2026-08-06 11:56:45 +0700

    Step 16: build_sft_trainer

--- model.py
+++ model.py
@@ -192,8 +192,22 @@ def build_training_arguments(output_dir='./sft_out', max_steps=5, learning_rate=
 
     return args
 
-# Step 16 - build_sft_trainer (not yet solved)
-# TODO: implement
+# Step 16 - build_sft_trainer
+from trl import SFTTrainer 
+
+def build_sft_trainer(model, tokenizer, dataset, training_args, max_seq_length=256):
+    """Construct a trl SFTTrainer over dataset['text'] ready to .train()."""
+    # TODO: wire model, tokenizer, dataset, and training_args into an SFTTrainer
+    sfttrainer = SFTTrainer(
+        model = model,
+        tokenizer = tokenizer,
+        train_dataset = dataset,
+        args = training_args,
+        max_seq_length = max_seq_length,
+        packing = False,
+        dataset_text_field = "text",
+    )
+    return sfttrainer
 
 # Step 17 - run_sft_training (not yet solved)
 # TODO: implement
